a2e9ea47bcefafcc1fa836346522a984b83cad71
leotabosa <leonardoctbraga@gmail.com>
2023-09-15 19:52:53 -0300

    Fix build

--- webapp/src/js/clientApi.js
+++ webapp/src/js/clientApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.API_URL}:3000`,
+  baseURL: process.env.API_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "*/*",
