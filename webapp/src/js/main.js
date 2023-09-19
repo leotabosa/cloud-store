b333cb3e9ec82ef0569a326274273897668387e2
leotabosa <leonardoctbraga@gmail.com>
2023-09-18 21:36:27 -0300

    fix: svg import on build

--- webapp/src/js/main.js
+++ webapp/src/js/main.js
@@ -1,2 +1,7 @@
 import * as bootstrap from "bootstrap";
 import "../scss/styles.scss";
+import landingPageSVG from "../assets/landing-page.svg";
+
+window.onload = () => {
+  document.getElementById("landing-page-svg").data = landingPageSVG;
+};
